f532ba824dd1e49dfc4ff3cdc9667da0db76e5bb
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-31 09:05:17 +0100

    - created HomeTableViewCell to show the each birthday of each person as list view in the home page

---
+++ BirthDates/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  BirthDates
+//
+//  Created by Sukumar Anup Sukumaran on 31/05/2022.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
