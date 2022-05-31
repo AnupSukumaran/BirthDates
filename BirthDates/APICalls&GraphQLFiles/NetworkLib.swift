57b726cc7527ecd5cc6c209f4754f6dff54be25c
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-31 09:34:45 +0100

    - Created the NetworkAPI class to call the GraphQL API using a function.

--- BirthDates/APICalls&GraphQLFiles/NetworkLib.swift
+++ BirthDates/APICalls&GraphQLFiles/NetworkLib.swift
@@ -6,8 +6,25 @@
 //
 
 import Foundation
+import Apollo
 
+enum Results<T> {
+    case success(T)
+    case failure(errorStr: String, model: T? = nil)
+}
 
 class NetworkLib {
+    static let shared = NetworkLib()
+    lazy var apollo = ApolloClient(url: URL(string: "https://birthday-api.hasura.app/v1/graphql")!)
     
+    func callBirthdaysAPI(comp: @escaping (Results<[BirthdaysQuery.Data.Person]>) -> Void) {
+        apollo.fetch(query: BirthdaysQuery()) { result in
+            switch result {
+            case .success(let graphQLResult):
+                comp(.success(graphQLResult.data?.person ?? []))
+            case .failure(let error):
+                comp(.failure(errorStr: error.localizedDescription))
+            }
+        }
+    }
 }
