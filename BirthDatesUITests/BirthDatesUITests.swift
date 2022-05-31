4df458719b86f6408c36fbc050662fd448ed08f2
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-31 10:54:17 +0100

    - Wrote UI test

--- BirthDatesUITests/BirthDatesUITests.swift
+++ BirthDatesUITests/BirthDatesUITests.swift
@@ -10,24 +10,34 @@ import XCTest
 class BirthDatesUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIApplication().launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAppFlow() throws {
+        
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let frontTxt = app.staticTexts["Birthdays"]
+        XCTAssertTrue(frontTxt.exists)
+        
+        let homeNav = app.navigationBars["Birthdays"]
+        expectation(for: NSPredicate(format: "exists == 1"), evaluatedWith: homeNav, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        if (app.tables.element(boundBy: 0).cells.count > 0) {
+            app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+            let backBtn = app.buttons["GO BACK"]
+            expectation(for: NSPredicate(format: "exists == 1"), evaluatedWith: backBtn, handler: nil)
+            waitForExpectations(timeout: 5, handler: nil)
+            backBtn.tap()
+            
+        }
+        
     }
 
     func testLaunchPerformance() throws {
