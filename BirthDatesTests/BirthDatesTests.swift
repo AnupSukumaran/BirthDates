8dcfadb147595eafdbdf9abea3979ee98eea25db
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-31 10:50:22 +0100

    - Wrote tests for necessary functions declared in the String extensions.

--- BirthDatesTests/BirthDatesTests.swift
+++ BirthDatesTests/BirthDatesTests.swift
@@ -9,21 +9,24 @@ import XCTest
 @testable import BirthDates
 
 class BirthDatesTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testGetInitials() throws {
+        let initials = "Micah Mayer".getInitials()
+        XCTAssertEqual(initials, "MM")
+    }
+    func testCheckDateFormate() throws {
+        let date = "2020-05-31".convertDateFormate()
+        XCTAssertEqual(date, "31-05-2020")
+    }
+    func testAgeValue() throws {
+        let date = "2020-05-31".ageValue()
+        XCTAssertEqual(date, "2 year(s) old")
     }
 
     func testPerformanceExample() throws {
@@ -32,5 +35,6 @@ class BirthDatesTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
 
 }
