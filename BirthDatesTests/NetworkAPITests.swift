5aa405190136252346e848dd13090011fc568b39
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2022-05-31 10:47:09 +0100

    - Wrote unit test for the API class

---
+++ BirthDatesTests/NetworkAPITests.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkAPITests.swift
+//  BirthDatesTests
+//
+//  Created by Sukumar Anup Sukumaran on 31/05/2022.
+//
+
+import XCTest
+@testable import BirthDates
+
+class NetworkAPITests: XCTestCase {
+
+    var sut: NetworkLib!
+    
+    override func setUpWithError() throws {
+        sut = NetworkLib()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testCallBirthdaysAPI() throws {
+        
+        let expect = XCTestExpectation(description: "callback")
+        
+        sut.callBirthdaysAPI { result in
+            expect.fulfill()
+            switch result {
+            case .success(let personArr):
+                XCTAssertNotNil(personArr)
+                
+            case .failure(errorStr: let err, _):
+                XCTFail(err)
+            }
+        }
+        
+        wait(for: [expect], timeout: 3.1)
+    }
+
+
+}
